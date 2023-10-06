d3dfe7e0f851fe17415482496baea760533ab098
Matthew Sanchez <mattsanchez1215@gmail.com>
2023-10-06 14:48:39 -0400

    create createItem controller

---
+++ controllers/clothingItems.js
@@ -0,0 +1,22 @@
+const { default: mongoose } = require("mongoose");
+const ClothingItem = require("../models/clothingItem");
+
+const createItem = (req, res) => {
+  console.log(req);
+  console.log(req.body);
+
+  const { name, weather, imageURL } = req.body;
+
+  ClothingItem.create({ name, weather, imageURL })
+    .then((item) => {
+      console.log(item);
+      res.send({ data: item });
+    })
+    .catch((e) => {
+      res.status(500).send({ message: "Error with createItem", e });
+    });
+};
+
+module.exports = {
+  createItem,
+};
