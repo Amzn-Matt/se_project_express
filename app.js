f7b6e75d9d56ffb70ffca198877e2e160887450c
Matthew Sanchez <mattsanchez1215@gmail.com>
2023-10-06 15:50:47 -0400

    fix app initialization

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const { PORT = 3001 } = process.env;
+const app = express();
 
 mongoose.connect(
   "mongodb://127.0.0.1:27017/wtwr_db",
@@ -10,11 +11,10 @@ mongoose.connect(
   (e) => console.log("DB ERROR", e),
 );
 
-const routes = require("./routes/index");
-app.use(routes);
+const routes = require("./routes");
 app.use(express.json());
+app.use(routes);
 
-const app = express();
 app.listen(PORT, () => {
   console.log(`App is listening at ${PORT}`);
 });
