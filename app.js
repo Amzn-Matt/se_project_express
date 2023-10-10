a9578576a67e923e89274a7baf3551ab3a303ef8
Matthew Sanchez <mattsanchez1215@gmail.com>
2023-10-09 21:35:39 -0400

    fix app error and install helmet

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const helmet = require("helmet");
+
 const { PORT = 3001 } = process.env;
 const app = express();
 
 mongoose.connect(
   "mongodb://127.0.0.1:27017/wtwr_db",
-  (r) => {
+  () => {
     console.log("DB is connected");
   },
   (e) => console.log("DB ERROR", e),
 );
 
 const routes = require("./routes");
+
 app.use(express.json());
+
+app.use(helmet());
 
 app.use((req, res, next) => {
   req.user = {
