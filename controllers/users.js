79d8f56f1025dc1c94ff051498f5abd039960682
Matthew Sanchez <mattsanchez1215@gmail.com>
2023-10-09 21:44:08 -0400

    refactor error handling

--- controllers/users.js
+++ controllers/users.js
@@ -1,16 +1,11 @@
-const mongoose = require("mongoose");
 const User = require("../models/user");
 const { BAD_REQUEST, NOT_FOUND, DEFAULT_ERROR } = require("../utils/errors");
 
 const createUser = (req, res) => {
-  console.log(req);
-  console.log(req.body);
-
   const { name, avatar } = req.body;
 
   User.create({ name, avatar })
     .then((item) => {
-      console.log(item);
       res.status(201).send({ data: item });
     })
     .catch((e) => {
@@ -19,35 +14,34 @@ const createUser = (req, res) => {
           message: `Error ${e.name} with the message ${e.message} has occurred while executing the code`,
         });
       } else {
-        res.status(DEFAULT_ERROR).send({ message: "Error with createUser", e });
+        res.status(DEFAULT_ERROR).send({ message: "Error with createUser" });
       }
     });
 };
 
 const getUsers = (req, res) => {
   User.find({})
-    .orFail()
     .then((item) => {
       res.status(200).send({ data: item });
     })
-    .catch((e) => {
-      res.status(DEFAULT_ERROR).send({ message: "Error with getUsers", e });
+    .catch(() => {
+      res.status(DEFAULT_ERROR).send({ message: "Error with getUsers" });
     });
 };
 
 const getUser = (req, res) => {
-  const userId = req.params.userId;
-
-  User.findById(userId)
+  User.findById(req.params.userId)
     .orFail()
     .then((item) => {
       res.status(200).send({ data: item });
     })
     .catch((e) => {
-      if (e.name === "CastError") {
+      if (e.name === "DocumentNotFoundError") {
         res.status(NOT_FOUND).send({ message: "User not found" });
+      } else if (e.name === "CastError") {
+        res.status(BAD_REQUEST).send({ message: "Invalid User ID" });
       } else {
-        res.status(DEFAULT_ERROR).send({ message: "Error with getUser", e });
+        res.status(DEFAULT_ERROR).send({ message: "Error with getUser" });
       }
     });
 };
