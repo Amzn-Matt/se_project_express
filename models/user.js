252d5ca3382c39463ccf846da1622bfa3757a2bd
Matthew Sanchez <mattsanchez1215@gmail.com>
2023-10-20 16:10:11 -0400

    add email and password to user schema and validate

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcrypt");
 
 const user = new mongoose.Schema({
   name: {
@@ -16,6 +17,44 @@ const user = new mongoose.Schema({
       message: "You must enter a valid URL",
     },
   },
+  email: {
+    type: String,
+    requred: true,
+    unique: true,
+    validate: {
+      validator(value) {
+        return validator.isEmail(value);
+      },
+      message: "You must enter a valid email",
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minlenght: 8,
+    select: false,
+  },
 });
+
+user.statics.findUserByCredentials = function findUserByCredentials(
+  email,
+  password,
+) {
+  return this.findOne({ email })
+    .select("+password")
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error("Username or Password are incorrect"));
+      }
+      return bcrypt.compare(password, user.password).then((matched) => {
+        if (!matched) {
+          return Promise.reject(
+            new Error("Username or Password are incorrect"),
+          );
+        }
+        return user;
+      });
+    });
+};
 
 module.exports = mongoose.model("users", user);
