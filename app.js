b1a67304d7da8ae4850d228a39f44496ea9b6bf3
Matthew Sanchez <mattsanchez1215@gmail.com>
2023-10-22 13:05:19 -0400

    remove hard coded user ID

--- app.js
+++ app.js
@@ -19,13 +19,6 @@ app.use(express.json());
 
 app.use(helmet());
 
-app.use((req, res, next) => {
-  req.user = {
-    _id: "6522e28868c73a44d78433d8", // paste the _id of the test user created in the previous step
-  };
-  next();
-});
-
 app.use(routes);
 
 app.listen(PORT, () => {
