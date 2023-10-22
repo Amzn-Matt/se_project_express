9e5c9bbbdbcf1eb38b3ef6198b295ef71186a63f
Matthew Sanchez <mattsanchez1215@gmail.com>
2023-10-22 13:05:59 -0400

    fix createUser

--- controllers/users.js
+++ controllers/users.js
@@ -18,28 +18,24 @@ const createUser = (req, res) => {
         .status(403)
         .send({ message: "A user with that email alreadt exists" });
     }
-  });
-
-  bcrypt
-    .hash(password, 10)
-    .then((hash) => {
-      User.create({ name, avatar, email, password: hash }).then((newUser) => {
-        res.status(201).send({
-          name: newUser.name,
-          avatar: newUser.email,
-          avatar: newUser.avatar,
+    return bcrypt
+      .hash(password, 10)
+      .then((hash) => {
+        User.create({ name, avatar, email, password: hash }).then((newUser) => {
+          res.status(201).send({ data: newUser });
         });
+      })
+      .catch((e) => {
+        console.error(err);
+        if (e.name === "DocumentNotFoundError") {
+          res.status(NOT_FOUND).send({ message: "User not found" });
+        } else if (e.name === "CastError") {
+          res.status(BAD_REQUEST).send({ message: "Invalid User ID" });
+        } else {
+          res.status(DEFAULT_ERROR).send({ message: "Error with createUser" });
+        }
       });
-    })
-    .catch((e) => {
-      if (e.name === "ValidationError") {
-        res.status(BAD_REQUEST).send({
-          message: `Error ${e.name} with the message ${e.message} has occurred while executing the code`,
-        });
-      } else {
-        res.status(DEFAULT_ERROR).send({ message: "Error with createUser" });
-      }
-    });
+  });
 };
 
 const login = (req, res) => {
