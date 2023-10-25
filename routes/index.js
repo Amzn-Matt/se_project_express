b090c93cbf1c125a117970fb98eacb7ea6dfd62d
Matthew Sanchez <mattsanchez1215@gmail.com>
2023-10-24 21:08:23 -0400

    remove authorization

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,10 @@ const clothingItems = require("./clothingItems");
 const users = require("./users");
 const { createUser, login } = require("../controllers/users");
 const { NOT_FOUND } = require("../utils/errors");
-const { authorize } = require("../middleware/auth");
 
-router.use("/items", authorize, clothingItems);
+router.use("/items", clothingItems);
 
-router.use("/users", authorize, users);
+router.use("/users", users);
 
 router.post("/signup", createUser);
 
