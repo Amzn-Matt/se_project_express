567f6c80e1aa01c6b028cdcde8cd98c6524758a2
Matthew Sanchez <mattsanchez1215@gmail.com>
2023-11-21 20:20:24 -0500

    create update user validation middleware

--- middleware/validation.js
+++ middleware/validation.js
@@ -54,6 +54,21 @@ module.exports.validateLoginBody = celebrate({
   }),
 });
 
+module.exports.validateUpdateProfile = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30).messages({
+      "string.min": 'The minimum length of the "name" field is 2',
+      "string.max": 'The maximum length of the "name" field is 30',
+      "string.empty": 'The "name" field must be filled in',
+    }),
+
+    avatar: Joi.string().required().custom(validateURL).messages({
+      "string.empty": 'The "imageUrl" field must be filled in',
+      "string.uri": 'the "imageUrl" field must be a valid url',
+    }),
+  }),
+});
+
 module.exports.validateId = celebrate({
   params: Joi.object().keys({
     itemId: Joi.string().hex().max(24),
